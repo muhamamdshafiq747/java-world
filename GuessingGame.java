cb60f2ea6d4b5142360ffec645122de6c6ed10c0
muhamamdshafiq747 <74946046+muhamamdshafiq747@users.noreply.github.com>
2020-11-24 16:38:45 +0800

    Add files via upload
    Without using arrays or regular expressions, write a Java program to implement the game MasterMindTM using only strings, characters and integers. Use the Scanner class to read user input and the Math class to generate random number.
    For the game, you must adhere to the following constraints:
     Your program makes 4 random choices to pick 4 different colours from 6 colours (stored as a string "RGBYOP"). The letters represent the 6 distinct colours: Red, Green, Blue, Yellow, Orange and Purple. The random choices are stored as a single string e.g., "YBRP".
     The player’s guess is read as a string and consists of 4 different letters for 4 different colours e.g., "BORY". Each correct colour guessed in the right position earns a black pin (B) and each correct colour guessed in a wrong position earns a white pin (W). In this example, the guess earns 2 white pins and 1 black pin, and the program displays WWB.
     The player wins if he is able to guess the 4 correct colours in the correct positions within 8 attempts.
     The player is allowed to quit a game at any time by entering q when prompted for a guess.
     At the end of a game, the program prompts the player whether he wants to play a new game. The user should enter q if he wishes to exit the program.
     Outline the control structures of structured programming and use ALL three repetition structures: for, while and do-while..
    An example run is shown below:
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: YPGO
    Result : WWB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: YGPR
    Result : WW
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: GPOB
    Result : WWB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: YPOB
    Result : WWB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: YOBG
    Result : WWWW
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: GOYB
    Result : WWWB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: GYBO
    Result : WWWB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: OBPG
    Result : WWB
    Sorry, you have already made 8 guesses
    The answer is OYGB
    Enter to play again or Q to quit:
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: GBPY
    Result : WWWB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: GYPB
    Result : WWBB
    Enter your guess using 4 distinct letters from RGBYOP and Q to quit: GYBP
    Result : BBBB
    Congratulations! You have guessed correctly in 3 attempts
    Enter to play again or Q to quit: q
    Hope you had fun!

---
+++ GuessingGame.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+        String playAgain = "Q";
+        do
+        {
+            String colours = "RBGYOP";
+            int randomNum;
+            String guessColours;
+            String ans = "";
+            String userguess;
+            String result="";
+            
+            
+            // I will use the below for loop to generate a random 4colour string.  
+            for(int x=6; x>2; x--)
+            {
+                randomNum =((char)(Math.random()*x));
+                guessColours = colours.substring(randomNum,randomNum+1);
+                colours=colours.substring(0,randomNum)+colours.substring(randomNum+1,colours.length());
+                ans = ans + guessColours;
+            }
+            System.out.println("The Random generated 4 colours are : " + ans);// if need to reveal ans for testing purpose 
+            int i =1;
+            while(i<9)
+            {   
+                System.out.print("Enter your guess using 4 distinct letters from RGBYOP and Q to quit: ");
+                userguess = sc.nextLine();
+              if(userguess.indexOf("Q")<= -1)
+              {
+                for(int x=0; x<ans.length(); x++)
+                { 
+                    if(ans.substring(x,x+1).equals(userguess.substring(x,x+1))) 
+                    {
+                        //System.out.print("B");
+                        result = result + "B";
+                    }
+                    else
+                    { 
+                        int index = ans.indexOf(userguess.substring(x,x+1));
+                        if(index>=0)
+                        {
+                            //System.out.print("W");
+                            result = result + "W";
+                        }
+
+                    }
+                }
+                  
+                System.out.println("Results : "+result);
+                if(result.equals("BBBB"))
+                {
+                    System.out.println("Congratulations! You have guessed correctly in "+i+" attempts");
+                    break; 
+                }
+                else
+                {  
+                    if(i>=8)
+                    {
+                        System.out.println("Sorry, you have already made 8 guesses");
+                        System.out.println("The answer is "+ans);
+                    }
+                    else{result="";}//Reset result to empty. 
+                }
+              }
+              else
+              {
+                System.out.println("You have enter Q to quit game.");//To inform user he has quit by entering q
+                break;
+              }
+              i++;
+            }
+            System.out.print("Enter to play again or Q to quit: "); // propmt if he wants to playagain when the game comes to an end/ or if he has quit
+         playAgain = sc.nextLine();
+        }while(!playAgain.equals("Q"));  
+        System.out.print("Hope you had fun !");
+        System.out.println();
+       
+
+        
+        
+ }
+}
+
