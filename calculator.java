a7afe67d47fa18edb48529702e230b0796da9b7c
muhamamdshafiq747 <74946046+muhamamdshafiq747@users.noreply.github.com>
2020-11-24 14:45:22 +0800

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,37 @@
+
+
+import java.util.Scanner;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double num1,num2,ans;
+		char operator; 
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Emter numbers : ");
+		num1 = reader.nextDouble();
+		num2 = reader.nextDouble();
+		System.out.println(" Enter the operator (+ - * /) : ");
+		operator = reader.next().charAt(0);
+		
+		switch(operator) {
+			case '+': ans = num1 + num2;
+				break;
+			case '-': ans = num1 - num2;
+				break;
+			case '*': ans = num1 * num2;
+				break;
+			case '/': ans = num1 / num2;
+				break;
+			default: System.out.printf("Error! Enter correct operator");
+			return;
+		}
+		
+		System.out.print("\nThe result is given as follows:\n");
+	    System.out.printf(num1 + " " + operator + " " + num2 + " = " + ans);
+	}
+
+}
